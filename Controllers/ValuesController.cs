0d824888acb98fe37861b96cf850627b81c975dc
Eduardo Cabrera <r.eduardo.cabrera@hotmail.com>
2020-08-20 08:49:21 -0600

    other new value4

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace WebAPIModulo10I.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2, value3" };
+            return new string[] { "value1", "value2, value3, value4" };
         }
 
         // GET api/<ValuesController>/5
