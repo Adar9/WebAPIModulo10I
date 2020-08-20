0b063f82a8eaeadc5a5c7761cd7b8817e8042564
Eduardo Cabrera <r.eduardo.cabrera@hotmail.com>
2020-08-20 08:28:47 -0600

    New value in valuescontyroller

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace WebAPIModulo10I.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2, value3" };
         }
 
         // GET api/<ValuesController>/5
